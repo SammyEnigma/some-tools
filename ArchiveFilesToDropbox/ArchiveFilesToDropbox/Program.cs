61cc5456551d59f60195725d2dc26f7c9e6418a0
Benedict Chan <purple.enchanted@gmail.com>
2016-12-09 16:18:10 +1100

    Project setup with logs

---
+++ ArchiveFilesToDropbox/ArchiveFilesToDropbox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace ArchiveFilesToDropbox
+{
+    class Program
+    {
+        private static ILogger Logger;
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Environment.SetEnvironmentVariable("BASEDIR", AppDomain.CurrentDomain.BaseDirectory);
+            Logger = new LoggerConfiguration().ReadFrom.AppSettings().CreateLogger();
+
+            Logger.Information("Test:{0}", "hello");
+        }
+    }
+}
