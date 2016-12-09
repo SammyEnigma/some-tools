070b14d3876466cff54192f152b0516242842fa9
Benedict Chan <purple.enchanted@gmail.com>
2016-12-09 17:28:19 +1100

    get folder and file

--- ArchiveFilesToDropbox/ArchiveFilesToDropbox/Program.cs
+++ ArchiveFilesToDropbox/ArchiveFilesToDropbox/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Serilog;
 
@@ -17,7 +20,24 @@ namespace ArchiveFilesToDropbox
             Environment.SetEnvironmentVariable("BASEDIR", AppDomain.CurrentDomain.BaseDirectory);
             Logger = new LoggerConfiguration().ReadFrom.AppSettings().CreateLogger();
 
-            Logger.Information("Test:{0}", "hello");
+            try
+            {
+                var archiveFolderPath = ConfigurationManager.AppSettings["archiveFolder"];
+                int takeFile = int.Parse(ConfigurationManager.AppSettings["noOfLatestFileToArchive"] ?? "1");
+                var archiveFolder = new DirectoryInfo(archiveFolderPath);
+                Logger.Information("Getting archive folder:{archiveFolder}", archiveFolder);
+                var matchingFiles = archiveFolder.GetFiles().OrderByDescending(f => f.LastWriteTime).Take(takeFile);
+
+                foreach (var file in matchingFiles)
+                {
+                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "");
+            }
         }
     }
 }
