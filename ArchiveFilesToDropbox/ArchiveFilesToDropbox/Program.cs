c26385cd3f620035299f1219b457d2ab9b58f3f3
Benedict Chan <purple.enchanted@gmail.com>
2016-12-13 10:22:42 +1100

    Add Trash folder rather than really delete the files, add settings for simulate but not remove or upload

--- ArchiveFilesToDropbox/ArchiveFilesToDropbox/Program.cs
+++ ArchiveFilesToDropbox/ArchiveFilesToDropbox/Program.cs
@@ -25,9 +25,13 @@ namespace ArchiveFilesToDropbox
             try
             {
                 var archiveFolderPath = ConfigurationManager.AppSettings["ArchiveFolder"];
+                var trashFolder = ConfigurationManager.AppSettings["TrashFolder"];
+                bool simulateOnly = bool.Parse(ConfigurationManager.AppSettings["SimulateOnly"] ?? "true");
+
                 int noOfFileToTake = int.Parse(ConfigurationManager.AppSettings["NoOfLatestFileToArchive"] ?? "1");
                 int noOfFileToKeep = int.Parse(ConfigurationManager.AppSettings["NoOfLatestFileToKeep"] ?? "5");
                 string dropboxAccessToken = ConfigurationManager.AppSettings["DropboxAccessToken"];
+                string dropboxFolderToArchive = ConfigurationManager.AppSettings["DropboxFolderToArchive"];
 
 
                 var archiveFolder = new DirectoryInfo(archiveFolderPath);
@@ -41,17 +45,28 @@ namespace ArchiveFilesToDropbox
                 {
                     Logger.Information("File to be archived:{0}", fileToAchive.FullName);
 
-                    var dbx = new DropboxClient(dropboxAccessToken);
-                    var dbFileMetaData = UploadFileToDropbox(dbx, fileToAchive.FullName, string.Empty, fileToAchive.Name).Result;
-                    Logger.Information("Uploaded File Success");
+                    if (!simulateOnly)
+                    {
+                        var dbx = new DropboxClient(dropboxAccessToken);
+                        var dbFileMetaData = UploadFileToDropbox(dbx, fileToAchive.FullName, dropboxFolderToArchive, fileToAchive.Name).Result;
+                        Logger.Information("Uploaded File Success");
+                    }
                     //Logger.Information("Uploaded File Info:{@dbFileMetaData}", dbFileMetaData);
 
                 }
 
+                if (!Directory.Exists(trashFolder))
+                {
+                    Directory.CreateDirectory(trashFolder);
+                }
                 foreach (var fileToDelete in deleteFiles)
                 {
-                    Logger.Warning("File to be deleted:{0}", fileToDelete.FullName);
-                    fileToDelete.Delete();
+                    Logger.Warning("File to be trashed:{0}", fileToDelete.FullName);
+                    if (!simulateOnly)
+                    {
+                        string moveToLocation = Path.Combine(trashFolder, fileToDelete.Name);
+                        fileToDelete.MoveTo(moveToLocation);
+                    }
                 }
 
 
