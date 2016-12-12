db9ffc221a090c82b3449ad6c0b0f8354ad2b753
Benedict Chan <purple.enchanted@gmail.com>
2016-12-12 16:58:13 +1100

    Add some code to upload file to dropbox, change config variable names

--- ArchiveFilesToDropbox/ArchiveFilesToDropbox/Program.cs
+++ ArchiveFilesToDropbox/ArchiveFilesToDropbox/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Dropbox.Api;
+using Dropbox.Api.Files;
 using Serilog;
 
 namespace ArchiveFilesToDropbox
@@ -22,9 +24,11 @@ namespace ArchiveFilesToDropbox
 
             try
             {
-                var archiveFolderPath = ConfigurationManager.AppSettings["archiveFolder"];
-                int noOfFileToTake = int.Parse(ConfigurationManager.AppSettings["noOfLatestFileToArchive"] ?? "1");
-                int noOfFileToKeep = int.Parse(ConfigurationManager.AppSettings["noOfLatestFileToKeep"] ?? "5");
+                var archiveFolderPath = ConfigurationManager.AppSettings["ArchiveFolder"];
+                int noOfFileToTake = int.Parse(ConfigurationManager.AppSettings["NoOfLatestFileToArchive"] ?? "1");
+                int noOfFileToKeep = int.Parse(ConfigurationManager.AppSettings["NoOfLatestFileToKeep"] ?? "5");
+                string dropboxAccessToken = ConfigurationManager.AppSettings["DropboxAccessToken"];
+
 
                 var archiveFolder = new DirectoryInfo(archiveFolderPath);
                 Logger.Information("Getting archive folder:{archiveFolder}", archiveFolder);
@@ -36,18 +40,60 @@ namespace ArchiveFilesToDropbox
                 foreach (var fileToAchive in archiveFiles)
                 {
                     Logger.Information("File to be archived:{0}", fileToAchive.FullName);
+
+                    var dbx = new DropboxClient(dropboxAccessToken);
+                    var dbFileMetaData = UploadFileToDropbox(dbx, fileToAchive.FullName, string.Empty, fileToAchive.Name).Result;
+                    Logger.Information("Uploaded File Success");
+                    //Logger.Information("Uploaded File Info:{@dbFileMetaData}", dbFileMetaData);
+
                 }
 
                 foreach (var fileToDelete in deleteFiles)
                 {
                     Logger.Warning("File to be deleted:{0}", fileToDelete.FullName);
+                    fileToDelete.Delete();
                 }
+
+
+
+                //Console.ReadLine();
 
             }
             catch (Exception ex)
             {
                 Logger.Error(ex, "");
+                //Console.ReadLine();
+
             }
         }
+
+
+
+
+        private static async Task<FileMetadata> UploadFileToDropbox(DropboxClient dbx, string localFilePath, string dropboxFolder, string fileName)
+        {
+
+            using (MemoryStream fileStream = new MemoryStream(File.ReadAllBytes(localFilePath)))
+            {
+                string filterdFileName = fileName.Replace("-", "");
+                Logger.Information("Uploading:{0} to Dropbox", localFilePath);
+
+                string targetLocation = $"/{dropboxFolder}/{filterdFileName}";
+                if (string.IsNullOrEmpty(dropboxFolder))
+                {
+                    targetLocation = $"/{filterdFileName}";
+                }
+                Logger.Information("Target Dropbox path:{0}", targetLocation);
+                var uploadedFileInformation = await dbx.Files.UploadAsync(targetLocation, WriteMode.Overwrite.Instance, body: fileStream);
+                return uploadedFileInformation;
+            }
+
+        }
+
+
+
+
     }
+
 }
+
